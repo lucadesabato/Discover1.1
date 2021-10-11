c01beb0e4dc47b063909fc3d1ab49b965689c9f6
lucadesabato <81817732+lucadesabato@users.noreply.github.com>
2021-10-11 10:37:52 +0200

    Discover1.1

---
+++ discover.py
@@ -0,0 +1,240 @@
+#Disccover v 1.1
+import argparse
+import subprocess
+from pathlib import Path
+import os
+import glob
+
+def openFileAsTable(filename):
+    with open(filename) as table_in:
+        table_data = [[str(col).rstrip() for col in row.split('\t')] for row in table_in]
+    return table_data
+
+def getStxSubType():
+    subprocess.call("pwd", shell=True)
+    subprocess.call("blastn -query stx.fasta -db ../../discover/data/stx -task blastn -evalue 0.001 -out shigatoxin -outfmt '6 std sallseqid score nident positive gaps ppos qframe sframe qseq sseq qlen slen salltitles' -num_threads 8 -strand both -dust yes -max_target_seqs 1 -perc_identity 95.0", shell=True)
+    file = open("shigatoxin")
+    read = file.readlines()
+    stx = open("shigatoxin_fc", "w")
+    for c in read:
+        line = c.split("\t")
+        if float(line[2]) > 95 and float(line[3]) > 1200:
+            stx.write(line[1] + "\t")
+            stx.write(line[2] + "\t")
+            stx.write(line[3] + "\t")
+            stx.write(line[15] + "\t")
+            stx.write("\n")
+    stx.close()
+    file.close()
+    shigatoxin_typing = openFileAsTable("shigatoxin_fc")
+    if len(shigatoxin_typing) == 0:
+        str_shigatoxin_subtype = "No subtype match found"
+    else:
+        # get corresponding subtypes
+        str_shigatoxin_subtype = ""
+        shigatoxin_subtypes = []
+        shigatoxin_subtypes_raw = []
+        shigatoxin_types = openFileAsTable("../../discover/data/stx_subtypes")
+        for subtype in shigatoxin_typing:
+            blast_pident_100 = float(subtype[1]) == 100
+            if (blast_pident_100):
+                for item in shigatoxin_types:
+                    if item[0] == subtype[0] and item[1] not in shigatoxin_subtypes_raw:
+                        shigatoxin_subtypes.append(item[1])
+                        shigatoxin_subtypes_raw.append(item[1])
+       # partial matches
+        for subtype in shigatoxin_typing:
+            for item in shigatoxin_types:
+                if item[0] == subtype[0] and item[1] not in shigatoxin_subtypes_raw:
+                    if item[1][0:4] == "stx1":
+                        shigatoxin_subtypes.append(item[1] + "(" + str(float(subtype[1])) + ")")
+                        shigatoxin_subtypes_raw.append(item[1])
+                    if item[1][0:4] == "stx2":
+                        shigatoxin_subtypes.append(item[1] + "(" + str(float(subtype[1])) + ")")
+                        shigatoxin_subtypes_raw.append(item[1])
+        shigatoxin_subtypes.sort()
+        str_shigatoxin_subtype = " ".join(shigatoxin_subtypes)
+    return str_shigatoxin_subtype
+
+def getSeroGroup(sero_file, antigen):
+    subprocess.call("awk -F '\t' '$4>800 { print $2 FS $3 FS $4 FS $16 }' " + sero_file + " | sort -nrk 2 -nrk 3 > serogroup_fc", shell=True)
+    subprocess.call("awk -F , '!seen[$0]++' serogroup_fc > serogroup_fcd", shell=True)
+    sero_typing = openFileAsTable("serogroup_fcd")
+    if len(sero_typing) == 0:
+        serogroup = antigen + "?"
+    else:
+        serogroup = sero_typing[0][0][sero_typing[0][0].rfind(antigen):]
+    return serogroup
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', dest='directory', help='relative path to directory with FASTQ data')
+parser.add_argument('-i', dest='input', help='type of FASTQ data: ion (Ion Torrent), illumina (Illumina)')
+args = parser.parse_args()
+
+os.chdir('./'+args.directory)
+os.system("ln -s " + os.popen("which trimmomatic").read().strip() + " trimmomatic.jar")
+subprocess.call("mkdir Contigs/", shell=True)
+
+if args.input =='illumina':
+    for file in glob.glob("*_R1*"):
+        sample=file.split('_')[0]
+        p1_file=file
+        p2_file=file.replace('R1', 'R2')
+        find_p2=os.listdir('./')
+        if p2_file in find_p2:
+            # TRIMMING
+            subprocess.call(
+            "trimmomatic PE -phred33 " + p1_file + " " + p2_file + " trimmed_" + sample + "_1.fq " + sample+"_1unpaired trimmed_" + sample + "_2.fq " + sample+"_2unpaired"+" SLIDINGWINDOW:5:20 LEADING:3 TRAILING:3 MINLEN:36", shell=True)
+            # ASSEMBLY
+            subprocess.call(
+            "perl " + "../discover/scripts/spades.pl "+sample+"_spades_contigs "+sample+"_spades_contig_stats "+sample+"_spades_scaffolds "+sample+"_spades_scaffold_stats "+sample+"_spades_log NODE spades.py --disable-gzip-output --isolate --pe1-ff --pe1-1"+ " trimmed_" + sample + "_1.fq "+ "--pe1-2 trimmed_" + sample + "_2.fq",
+            shell=True)
+            subprocess.call(
+            "perl " + "../discover/scripts/filter_spades_repeats.pl -i "+sample+"_spades_contigs -t "+sample+"_spades_contig_stats -c 0.33 -r 1.75 -l 1000 -o "+sample+"_output_with_repeats -u "+sample+"_contigs -n "+sample+"_repeat_sequences_only -e 5000 -f "+sample+"_discarded_sequences -s "+sample+"_summary",
+            shell=True)
+            if len(sample + "_contigs") != 0:
+                Path(sample + "_disc").mkdir(exist_ok=True)
+                subprocess.call("mv " + "trimmed_" + sample + "_1.fq " + sample + "_disc/", shell=True)
+                subprocess.call("mv " + "trimmed_" + sample + "_2.fq " + sample + "_disc/", shell=True)
+                subprocess.call("cp " + sample + "_contigs Contigs/", shell=True)
+                subprocess.call("mv " + sample + "_contigs " + sample + "_disc/", shell=True)
+                subprocess.call("rm -rf output_dir", shell=True)
+        else:
+            log=open(sample+'_log.txt', 'w')
+            log.write(sample+'_R2: NOT FOUND')
+
+    subprocess.call("rm *spades*", shell=True)
+    subprocess.call("rm *unpaired", shell=True)
+    subprocess.call("rm *summary", shell=True)
+    subprocess.call("rm *sequences", shell=True)
+    subprocess.call("rm *sequences_only", shell=True)
+    subprocess.call("rm *repeats", shell=True)
+
+    for file in glob.glob("*_disc/"):
+        sample = file.split('_')[0]
+        os.chdir(file)
+
+    # VIRULOTYPER
+        subprocess.call("abricate --db virulotyper "+sample + "_contigs > virulotyper_results.txt", shell=True)
+
+    # SHIGATOXIN TYPER
+        subprocess.call(
+            "../../discover/scripts/stx_subtype_pe.sh ../.. " + "trimmed_" + sample + "_1.fq " + "trimmed_" + sample + "_2.fq " + sample + "_contigs",
+            shell=True)
+        results_stx = []
+        results_stx.append(getStxSubType())
+        stx = open("shigatoxin_results.txt", "w")
+        stx.write("SHIGATOXIN TYPER: " + "\n")
+        [stx.write(c + "\n") for c in results_stx]
+        stx.close()
+
+    # SEROTYPER O&H
+        subprocess.call("../../discover/scripts/serotype.sh ../.. y "+"trimmed_"+sample+"_1.fq "+ "trimmed_"+sample+"_2.fq "+sample+"_contigs",
+        shell=True)
+        results_o = []
+        results_h = []
+        results_o.append(getSeroGroup('serogroup_O', "O"))
+        results_h.append(getSeroGroup('serogroup_H', "H"))
+        serotyper = open("serotyper_results.txt", "w")
+        serotyper.write("SEROTYPER O&H " + "\n")
+        serotyper.write("SEROTYPER O: ")
+        [serotyper.write(c + ", ") for c in results_o]
+        serotyper.write("\n")
+        serotyper.write("SEROTYPER H: ")
+        [serotyper.write(c + ", ") for c in results_h]
+        serotyper.close()
+
+    # MLST
+        subprocess.call("mlst --scheme ecoli "+sample+"_contigs > mlst_results.txt", shell=True)
+
+    # AMRGENES, abrichate
+        subprocess.call("abricate -db ncbi "+sample+"_contigs > amr_abrichate_results.txt", shell=True)
+
+    # CHEwBBACCA
+        chewbbca=os.popen("which chewBBACA.py").read()
+        subprocess.call("mkdir chewbbca", shell=True)
+        subprocess.call("cp "+sample+"_contigs ./chewbbca/"+sample+"_contigs.fasta",shell=True)
+        subprocess.call("python "+chewbbca+"chewBBACA.py AlleleCall -i ./chewbbca/ -g  ../../discover/chewBBACA_db/schema_chewBBACA_cgMLST_V4 -o chewbacca_results/ --cpu 6 --bsr 0.6 --ptf ../../discover/chewBBACA_db/trained_eColi.trn --fr", shell=True)
+        subprocess.call("cp chewbacca_results/results_*/results_statistics.tsv ./chewbbca_results.tsv", shell=True)
+        subprocess.call("cp chewbacca_results/results_*/results_alleles.tsv ./results_alleles.tsv", shell=True)
+        subprocess.call("cp chewbacca_results/results_*/RepeatedLoci.txt ./RepeatedLoci.txt", shell=True)
+        os.chdir('../')
+
+if args.input=='ion':
+    list_ngs_data=glob.glob("*fastq")+glob.glob("*fastqsanger")
+    for file in list_ngs_data:
+        sample=file.split('_')[0]
+        # TRIMMING
+        subprocess.call("trimmomatic SE -phred33 " + file + " trimmed_" + sample + ".fq SLIDINGWINDOW:5:20 LEADING:3 TRAILING:3 MINLEN:55",
+        shell=True)
+        # ASSEMBLY
+        subprocess.call(
+        "perl " + "../discover/scripts/spades.pl "+sample+"_spades_contigs "+sample+"_spades_contig_stats "+sample+"_spades_scaffolds "+sample+"_spades_scaffold_stats "+sample+"_spades_log NODE spades.py --disable-gzip-output --isolate --iontorrent -s trimmed_" + sample + ".fq",
+        shell=True)
+        subprocess.call(
+        "perl " + "../discover/scripts/filter_spades_repeats.pl -i "+sample+"_spades_contigs -t "+sample+"_spades_contig_stats -c 0.33 -r 1.75 -l 1000 -o "+sample+"_output_with_repeats -u "+sample+"_contigs -n "+sample+"_repeat_sequences_only -e 5000 -f "+sample+"_discarded_sequences -s "+sample+"_summary",
+        shell=True)
+        if len(sample + "_contigs") != 0:
+            Path(sample + "_disc").mkdir(exist_ok=True)
+            subprocess.call("mv " + " trimmed_" + sample + ".fq " + sample + "_disc/", shell=True)
+            subprocess.call("cp " + sample + "_contigs Contigs/", shell=True)
+            subprocess.call("mv " + sample + "_contigs " + sample + "_disc/", shell=True)
+            subprocess.call("rm -rf output_dir", shell=True)
+    
+    subprocess.call("rm *spades*", shell=True)
+    subprocess.call("rm *summary", shell=True)
+    subprocess.call("rm *sequences", shell=True)
+    subprocess.call("rm *sequences_only", shell=True)
+    subprocess.call("rm *repeats", shell=True)
+
+    for file in glob.glob("*_disc/"):
+        sample = file.split('_')[0]
+        os.chdir(file)
+
+    # VIRULOTYPER
+        subprocess.call("abricate --db virulotyper "+sample + "_contigs > virulotyper_results.txt",
+        shell=True)
+
+    # SHIGATOXIN TYPER
+        subprocess.call("../../discover/scripts/stx_subtype_se.sh ../.. "+"trimmed_"+sample+".fq "+sample+"_contigs",
+        shell=True)
+        results_stx = []
+        results_stx.append(getStxSubType())
+        stx=open("shigatoxin_results.txt", "w")
+        stx.write("SHIGATOXIN TYPER: "+"\n")
+        [stx.write(c+"\n") for c in results_stx]
+        stx.close()
+
+    # SEROTYPER O&H
+        subprocess.call("../../discover/scripts/serotype.sh ../.. n trimmed_"+sample+".fq xxx "+sample+"_contigs",
+        shell=True)
+        results_o = []
+        results_h = []
+        results_o.append(getSeroGroup('serogroup_O', "O"))
+        results_h.append(getSeroGroup('serogroup_H', "H"))
+        serotyper = open("serotyper_results.txt", "w")
+        serotyper.write("SEROTYPER O&H " + "\n")
+        serotyper.write("SEROTYPER O: ")
+        [serotyper.write(c + ", ") for c in results_o]
+        serotyper.write("\n")
+        serotyper.write("SEROTYPER H: ")
+        [serotyper.write(c + ", ") for c in results_h]
+        serotyper.close()
+
+    # MLST
+        subprocess.call("mlst --scheme ecoli "+sample+"_contigs > mlst_results.txt", shell=True)
+
+    # AMRGENES, abrichate
+        subprocess.call("abricate -db ncbi "+sample+"_contigs > amr_abrichate_results.txt", shell=True)
+
+    #CHEwBBACCA
+        chewbbca=os.popen("which chewBBACA.py").read()
+        subprocess.call("mkdir chewbbca", shell=True)
+        subprocess.call("cp "+sample+"_contigs ./chewbbca/"+sample+"_contigs.fasta",shell=True)
+        subprocess.call("python "+chewbbca+"chewBBACA.py AlleleCall -i ./chewbbca/ -g  ../../discover/chewBBACA_db/schema_chewBBACA_cgMLST_V4 -o chewbacca_results/ --cpu 6 --bsr 0.6 --ptf ../../discover/chewBBACA_db/trained_eColi.trn --fr", shell=True)
+        subprocess.call("cp chewbacca_results/results_*/results_statistics.tsv ./chewbbca_results.tsv", shell=True)
+        subprocess.call("cp chewbacca_results/results_*/results_alleles.tsv ./results_alleles.tsv", shell=True)
+        subprocess.call("cp chewbacca_results/results_*/RepeatedLoci.txt ./RepeatedLoci.txt", shell=True)
+        os.chdir('../')
+
+subprocess.call("python ../discover_results.py", shell=True)
---
+++ discover_results.py
@@ -0,0 +1,245 @@
+#Disccover v 1.1
+import os
+import glob
+import csv
+import operator
+from numpy import mean
+
+#Tab_results.txt and  Tab_AMR.txt
+list_gene_fine=['asta', 'ipad', 'saa', 'agg3a', 'cif', 'espj', 'toxb', 'aggd', 'lpfa', 'cfac', 'agg3d', 'katp', 'espa', 'ltca', 'cnf1', 'espc', 'espb', 'aata', 'agg4c', 'tccp', 'stx1a', 'ipah9', 'lnga', 'tir', 'cdtb', 'iss', 'espf', 'tsh', 'espp', 'virf', 'agg5a', 'sfas', 'k88ab', 'bfpa', 'efa1', 'capu', 'agga', 'eila', 'pic', 'cma', 'vat', 'agg4d', 'agg4a', 'cci', 'siga', 'aar', 'stx2a', 'ehxa', 'mchb', 'senb', 'aafd', 'iha', 'aggc', 'stx2b', 'fim41a', 'etpd', 'fasa', 'mcma', 'nleb', 'aafa', 'nlea', 'sat', 'feda', 'nfae', 'iron', 'suba', 'pet', 'pera', 'aafc', 'air', 'agg3c', 'aaic', 'eae', 'epea', 'gad', 'sepa', 'rpea', 'hlye', 'fedf', 'aap', 'orf4', 'aggr', 'f17a', 'nlec', 'orf3', 'cba', 'fana', 'stb', 'aafb', 'mchc', 'eata', 'sta1', 'agg3b', 'mchf', 'aggb', 'stx1b', 'f17g', 'celb', 'cofa', 'espi', 'agg4b', 'irea']
+
+entries = os.listdir('./')
+if 'Tab_results.txt' not in entries:
+    tab1=open('Tab_results.txt', 'w')
+    prima_riga = 'Sample' + '\t' + 'Avg Scaffold coverage' + '\t' + 'Burst size' + '\t' + 'MLST' + '\t' + 'STX subtype' + '\t' + 'Serotype O' + '\t'+ 'Serotype H' + '\t'
+    for c in list_gene_fine:
+        prima_riga += c
+        prima_riga += '\t'
+    prima_riga+='\n'
+    tab1.write(prima_riga)
+else:
+    tab1 = open('Tab_results.txt', 'a')
+
+if 'Tab_AMR.txt' not in entries:
+    tab2 = open('Tab_AMR.txt', 'w')
+    prima_riga = 'Sample' + '\t' + 'AMR genes'
+    tab2.write(prima_riga)
+else:
+    tab2 = open('Tab_AMR.txt', 'a')
+
+if 'Tab_cgMLST.txt' not in entries:
+    tab3 = open('Tab_cgMLST.txt', 'w')
+    for file in glob.glob("*_disc/"):
+        prima_riga = ''
+        os.chdir(file)
+        prima_riga = 'Sample' + '\t'
+
+        # extract info for MLST gene results
+        csv_file = open("results_alleles.tsv")
+        read_csv = list(csv.reader(csv_file, delimiter="\t"))
+
+        for gene in read_csv[0][1:]:
+            prima_riga += gene + '\t'
+        prima_riga += "\n"
+
+        tab3.write(prima_riga)
+        os.chdir("../")
+        break
+else:
+    tab3 = open('Tab_cgMLST.txt', 'a')
+
+if 'Contamination_sheet.txt' not in entries:
+    tab4 = open('Contamination_sheet.txt', 'w')
+    prima_riga = 'Sample' + '\t' + 'Species'+ '\t' + 'Gene' + '\t' + 'PC' + '\t' + 'NDC' + '\n'
+    tab4.write(prima_riga)
+else:
+    tab4 = open('Contamination_sheet.txt', 'a')
+
+
+for file in glob.glob("*_disc/"):
+    namesample = file.split('_')[0]
+    os.chdir(file)
+    row_sample=namesample+'\t'
+
+    if os.path.isfile("./virulotyper_results.txt")==True:
+        #VIRULOTYPER RESULTS
+        #read the file virulotyper_results.txt
+        file1 = open('virulotyper_results.txt')
+        read_csv = list(csv.reader(file1, delimiter="\t"))
+
+        #list of genes with coverage >80.0
+        geni_ED=[]
+        for line in read_csv[1:]:
+            if float(line[8])>=80.0:
+                in_line=[]
+                in_line.append(line[12])
+                in_line.append(float(line[8]))
+                in_line.append(float(line[9]))
+                in_line.append(float(line[1].split('_')[5]))
+                geni_ED.append(in_line)
+
+        #order genes by coverage, identity and read mean covrage
+        ED_best_gene=[]
+        tot_mean=[]
+        for geni in list_gene_fine:
+            list_gene=[]
+            for line in geni_ED:
+                if line[0].split('_')[0]==geni:
+                    list_gene.append(line)
+                    tot_mean.append(line[3])
+
+            s = sorted(list_gene, key=operator.itemgetter(1, 2, 3), reverse=True)
+            if len(s)!=0:
+                ED_best_gene.append(s[0])
+
+        #coverage mean
+        mean_gene=str(round(mean(tot_mean)))
+
+        #empty list from list_gene_fine
+        find_gene=['0']*len(list_gene_fine)
+        for x in ED_best_gene:
+            gen = x[0].split('_')[0]
+            for c in list_gene_fine:
+                if gen==c:
+                    find_gene[list_gene_fine.index(c)]=x[0]
+        file1.close()
+    else:
+        find_gene = ['ND'] * len(list_gene_fine)
+
+    if os.path.isfile("./mlst_results.txt") == True:
+        #MLST
+        mlst=''
+        species=''
+        file2=open('mlst_results.txt').readlines()
+        for line in file2:
+            mlst+='ST'+line.split()[2]+'; '
+            species+=line.split()[1]+'; '
+    else:
+        mlst="ND"
+
+    if os.path.isfile("./chewbbca_results.tsv") == True:
+        #extract EXC+INF from chewBBACA
+        file3=open('chewbbca_results.tsv')
+        file3_line=file3.readlines()
+        exc=str(int(file3_line[1].split()[1])+int(file3_line[1].split()[2]))
+        file3.close()
+    else:
+        exc="ND"
+
+    if os.path.isfile("./shigatoxin_results.txt") == True:
+        # SHIGATOXIN TYPER
+        file4 = open("shigatoxin_results.txt")
+        file4_lines=file4.readlines()
+        list_of_stx = ''
+        if file4_lines[1].find("No subtype match found")==-1:
+            for line in file4_lines[1:]:
+                for c in line.strip('\n').split(' '):
+                    list_of_stx += c[0:4] + c[4].lower() + '; '
+        else:
+            list_of_stx+="NOT FOUND  "
+        file4.close()
+    else:
+        list_of_stx = "ND  "
+
+    if os.path.isfile("./serotyper_results.txt") == True:
+        # SEROTYPER- STX O&H
+        file5 = open("serotyper_results.txt")
+        file5_lines=file5.readlines()
+        sero_o = file5_lines[1].strip('\n').split(':')[1:][0].replace(',', ';')
+        sero_h = file5_lines[2].strip('\n').split(':')[1:][0].replace(',', ';')
+        file5.close()
+    else:
+        sero_o ="ND  "
+        sero_h ="ND  "
+
+    #add info to sample row
+    row_sample=namesample+'\t'
+    row_sample+=mean_gene+'\t'
+    row_sample+=exc+'\t'
+    row_sample+=mlst[:-2]+'\t'
+    row_sample += list_of_stx[:-2] + '\t'
+    row_sample += sero_o[:-2] + '\t'
+    row_sample +=sero_h[:-2] + '\t'
+
+    for gene in find_gene:
+        row_sample += str(gene) + '\t'
+
+    row_sample+='\n'
+    tab1.write(row_sample)
+
+    if os.path.isfile("./amr_abrichate_results.txt") == True:
+        #extract info for AMR results
+        csv_file = open("amr_abrichate_results.txt")
+        read_csv = list(csv.reader(csv_file, delimiter="\t"))
+        riga2 = namesample + ' :' + '\t'
+
+        #list of genes with coverage >80.0
+        geni_amr = []
+        list_geni_amr=[]
+        for line in read_csv[1:]:
+            if float(line[8]) >= 80.0:
+                in_line = []
+                in_line.append(line[4])
+                in_line.append(float(line[8]))
+                in_line.append(float(line[9]))
+                in_line.append(float(line[1].split('_')[5]))
+                geni_amr.append(in_line)
+                list_geni_amr.append(line[4])
+
+        list_geni_amr=list(set(list_geni_amr))
+
+        # order genes by coverage, identity and read mean covrage
+        ED_best_amr = []
+        for geni in list_geni_amr:
+            list_gene = []
+            for line in geni_amr:
+                if line[0] == geni:
+                    list_gene.append(line)
+
+            s = sorted(list_gene, key=operator.itemgetter(1, 2, 3), reverse=True)
+            if len(s) != 0:
+                ED_best_amr.append(s[0])
+
+        for x in ED_best_amr:
+            riga2 += x[0] + "; "
+
+    else:
+        riga2=namesample + ' :' + '\tND'+"\n"
+
+    csv_file.close()
+    tab2.write('\n' + riga2)
+
+    if os.path.isfile("./results_alleles.tsv") == True:
+        # extract info for MLST gene results
+        row_sample = namesample + '\t'
+        csv_file = open("results_alleles.tsv")
+        read_csv = list(csv.reader(csv_file, delimiter="\t"))
+
+        for value in read_csv[1][1:]:
+            row_sample += value + '\t'
+        row_sample += "\n"
+        csv_file.close()
+        tab3.write(row_sample)
+    else:
+        row_sample = namesample + '\tND'+"\n"
+        tab3.write(row_sample)
+
+    if os.path.isfile("./RepeatedLoci.txt") == True:
+        #MLST
+        file4=open('RepeatedLoci.txt').readlines()
+        row_sample = namesample + '\t' + species[:-2] + '\t'
+        if len(file4)==1:
+            row_sample+= '\t' + '\t' + '\t' + '\n'
+        else:
+            line=file4[1].split('\t')
+            row_sample+=line[0] + '\t' + line[1] + '\t' + line[2] + '\n'
+        tab4.write(row_sample)
+    os.chdir('../')
+
+tab1.close()
+tab2.close()
+tab3.close()
+tab4.close()
+
+
+
